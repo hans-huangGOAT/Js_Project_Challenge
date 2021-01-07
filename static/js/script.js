f716915de27f01bc2aab3ca811edd4853c98154f
Hans <fb_hans@outlook.com>
2021-01-07 10:12:14 +0800

    Blackjack Js Pt.3

--- static/js/script.js
+++ static/js/script.js
@@ -151,7 +151,9 @@ function randomColors() {
 let blackjackGame = {
     'you': { 'scoreSpan': '#your-blackjack-result', 'div': '#your-box', 'score': 0 },
     'dealer': { 'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0 },
-    'cards': ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A' ],
+    'cards': ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'],
+    'cardsMap': { '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'K': 10, 'J': 10, 'Q': 10, 'A': [1, 11] },
+
 }
 
 const YOU = blackjackGame['you'];
@@ -166,28 +168,36 @@ document.querySelector('#blackjack-hit-button').addEventListener('click', blackj
 document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
 
 function blackjackHit() {
+
     let card = randomCard();
     console.log(card);
     showCard(YOU, card);
+    updateScore(card, YOU);
+    console.log(YOU['score']);
+    showScore(YOU);
 }
 
-function randomCard(){
+function randomCard() {
     let randomIndex = Math.floor(Math.random() * 13);
     return blackjackGame['cards'][randomIndex];
-} 
+}
 
 
 function showCard(activePlayer, card) {
-    let cardImage = document.createElement('img');
-    /*
-    One way to do that is using string concatention
-    cardImage.src = 'static/images/' + card + '.png';
-    */
-   //notice that the string below is quoted using the sign "`"
-   //instead of single quote
-   cardImage.src = `static/images/${card}.png`;
-    document.querySelector(activePlayer['div']).appendChild(cardImage);
-    hitSound.play();
+    if (activePlayer['score'] <= 21) {
+        let cardImage = document.createElement('img');
+        /*
+        One way to do that is using string concatention
+        cardImage.src = 'static/images/' + card + '.png';
+        */
+        //notice that the string below is quoted using the sign "`"
+        //instead of single quote
+        cardImage.src = `static/images/${card}.png`;
+        document.querySelector(activePlayer['div']).appendChild(cardImage);
+        hitSound.play();
+    } else{
+         
+    }
 }
 
 function blackjackDeal() {
@@ -199,5 +209,35 @@ function blackjackDeal() {
     for (let i = 0; i < dealerImages.length; i++) {
         dealerImages[i].remove();
     }
+
+    YOU['score'] = 0;
+    DEALER['score'] = 0;
+
+    document.querySelector(YOU['scoreSpan']).textContent = 0;
+    document.querySelector(DEALER['scoreSpan']).textContent = 0;
+    document.querySelector(YOU['scoreSpan']).style.color = 'white';
+    document.querySelector(DEALER['scoreSpan']).style.color = 'white';
 }
 
+function updateScore(card, activePlayer) {
+    if (card === 'A') {
+        // if adding 11 keeps me below 21, add 11. Otherwise, add 1
+        if (activePlayer['score'] + blackjackGame['cardsMap'][card][1] <= 21) {
+            activePlayer['score'] += blackjackGame['cardsMap'][card][1];
+        } else {
+            activePlayer['score'] += blackjackGame['cardsMap'][card][0];
+        }
+    } else {
+        activePlayer['score'] += blackjackGame['cardsMap'][card];
+    }
+
+}
+
+function showScore(activePlayer) {
+    if (activePlayer['score'] > 21){
+        document.querySelector(activePlayer['scoreSpan']).textContent = 'BUST!';
+        document.querySelector(activePlayer['scoreSpan']).style.color = 'red';
+    }else{
+        document.querySelector(activePlayer['scoreSpan']).textContent = activePlayer['score'];
+    }
+}
