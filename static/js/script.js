a73765579e8a7dab1509f01e1e12ab5f96290201
Hans <fb_hans@outlook.com>
2021-01-08 22:24:08 +0800

    Small Changes

--- static/js/script.js
+++ static/js/script.js
@@ -339,3 +339,26 @@ function showResult(winner) {
         result.style.color = messageColor;
     }
 }
+
+//Challenge 6: AJAX & API's with Javascript
+// const url = 'https://randomuser.me/api/?results=10'
+
+// fetch(url)
+// .then(resp => resp.json())
+// .then(data => {
+//     let authors = data.results;
+//     console.log(authors)
+//     for(let i = 0; i < authors.length; i++){
+//         let div = document.createElement('div')
+//         let image = document.createElement('img')
+//         let p = document.createElement('p')
+//         p.appendChild(document.createTextNode(`${title(authors[i].name.first)} ${title(authors[i].name.last)}`));
+//         image.src = authors[i].picture.large;
+//         div.appendChild(image)
+//         div.appendChild(p)
+//         document.querySelector('.container-6 .flex-ajax-row-1').appendChild(div);
+
+//     }
+// });
+
+// let title = str => str[0].toUpperCase() + str.slice(1);
